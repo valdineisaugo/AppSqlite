80c010cb20ac38a9980eedaa505a8043857fda92
valdineisaugo <valdinei@saugo.com.br>
2021-06-23 21:56:18 -0300

    Update 23/06 - Versão com Firebase.

--- app/src/main/java/br/com/saugo/MainActivity.java
+++ app/src/main/java/br/com/saugo/MainActivity.java
@@ -8,6 +8,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.Date;
 import database.DataBaseHelper;
 
@@ -20,10 +24,15 @@ public class MainActivity extends AppCompatActivity {
     EditText editNome, editCpf, editSaldo;
     SQLiteDatabase db;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Teste com Firebase
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
         //Definição dos views
         editCpf = findViewById(R.id.editCpf);
         editNome = findViewById(R.id.editNome);
@@ -45,12 +54,18 @@ public class MainActivity extends AppCompatActivity {
                 values.put("cpf", cpf);
                 values.put("saldo", saldo);
                 long resultado = db.insert("cliente", null, values);
-                if(resultado != -1)
+                if (resultado != -1)
                     Toast.makeText(MainActivity.this, "Registro Salvo", Toast.LENGTH_LONG).show();
                 else
                     Toast.makeText(MainActivity.this, "Falha ao salvar", Toast.LENGTH_LONG).show();
+                //Inserir no FB
+                DatabaseReference myRef = database.getReference(new Date().toString());
+                myRef.child("nome").setValue(editNome.getText().toString());
+                myRef.child("saldo").setValue(editSaldo.getText().toString());
+                myRef.child("cpf").setValue(editCpf.getText().toString());
             }
         });
+
 
 
         //Listar Registros após insert
